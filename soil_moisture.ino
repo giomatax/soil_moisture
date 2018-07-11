e7b6631bcb50a0d2995e5c66f5ca281906ba0814
Giorgi Matakheria <giomatax@gmail.com>
2018-07-11 18:13:49 +0400

    Add files via upload

---
+++ soil_moisture.ino
@@ -0,0 +1,16 @@
+const int sensor_pin = A0;	//* პინი რომელზეც უნდა დაერთდეს მოწყობილობის ლოგიკური ფეხი ჩვენ შემთხვევაში  AO სადაც წერია
+
+void setup() {
+  Serial.begin(9600);	/* სერიალ მონიტორინგისთვის პორტი */
+}
+
+void loop() {
+  float moisture_percentage;
+  int sensor_analog;
+  sensor_analog = analogRead(sensor_pin);
+  moisture_percentage = ( 100 - ( (sensor_analog/1023.00) * 100 ) );
+  Serial.print("ტენიანობის პროცენტულობა = ");
+  Serial.print(moisture_percentage);
+  Serial.print("%\n\n");
+  delay(1000);
+}
